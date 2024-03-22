0ac4f364949d19e489eecf370506f96239dc47d9
RONAK SAINI <92773874+ronaksaini@users.noreply.github.com>
2024-03-22 10:56:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-// Connect to MongoDB
 mongoose
   .connect("mongodb://localhost:27017/ronak", {
     useNewUrlParser: true,
@@ -14,7 +13,7 @@ mongoose
   .then(() => console.log("Connected successfully to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-// Define a schema for the user data
+
 const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
@@ -32,16 +31,14 @@ const userSchema = new mongoose.Schema({
   updationTime: { type: Date, default: Date.now },
 });
 
-// Define a model based on the schema
+
 const User = mongoose.model("User", userSchema);
 
-// Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
 // Serve static files (HTML, JavaScript)
 app.use(express.static("public"));
 
-// Endpoint to get all submitted data
 app.get("/data", async (req, res) => {
   try {
     const allUsers = await User.find();
@@ -52,16 +49,16 @@ app.get("/data", async (req, res) => {
   }
 });
 
-// Endpoint to handle form submission
+
 app.post("/submit", async (req, res) => {
   try {
     const userData = req.body;
-    // Create a new user document using the model
+
     const newUser = new User(userData);
     // Save the user document to the database
     await newUser.save();
     console.log("Data inserted successfully");
-    res.json(newUser); // Return the inserted user data as JSON
+    res.json(newUser);
   } catch (err) {
     console.error("Error occurred while inserting data into MongoDB", err);
     res.status(500).send("Error occurred while inserting data into MongoDB");
